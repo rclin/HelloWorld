897ee4d91b0c3100140a04da35027b627bb6c9ce
Richard Lin <rilin@paypalcorp.com>
2015-12-04 16:12:20 -0800

    Initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,20 @@
+/**
+ * Created by rilin on 12/4/2015.
+ */
+public class Fibonacci {
+
+    public static void printFibonnaci() {
+
+        int lo = 1;
+        int hi = 1;
+
+        System.out.println(lo);
+
+        while (hi < 50) {
+            System.out.println(hi);
+            hi = lo + hi;
+            lo = hi - lo;
+        }
+    }
+
+}
---
+++ src/Fibonacci2.java
@@ -0,0 +1,22 @@
+/**
+ * Created by rilin on 12/4/2015.
+ */
+public class Fibonacci2 {
+
+    static final int MAX = 50;
+
+    public static void printFibonacci2() {
+
+        int lo = 1;
+        int hi = 1;
+
+        System.out.println(lo);
+
+        while (hi < MAX) {
+            System.out.println(hi);
+            hi = lo + hi;
+            lo = hi - lo;
+        }
+
+    }
+}
